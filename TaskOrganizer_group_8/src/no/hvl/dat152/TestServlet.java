ffb3c123a3eaedb4e7927afe7b687f20ebb7c2a2
182719 <joakim-a@hotmail.com>
2019-09-19 13:47:21 +0200

    Servlet

---
+++ TaskOrganizer_group_8/src/no/hvl/dat152/TestServlet.java
@@ -0,0 +1,15 @@
+package no.hvl.dat152;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "TestServlet")
+public class TestServlet extends javax.servlet.http.HttpServlet {
+
+	protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+	}
+
+	protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+	}
+}
