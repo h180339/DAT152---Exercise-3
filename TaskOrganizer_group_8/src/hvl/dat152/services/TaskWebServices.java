d5a04f7dd4cc0c69b71c2d0fa81bc0ab9b0a7e04
182719 <joakim-a@hotmail.com>
2019-09-20 10:43:46 +0200

    Server code from Bjarte is added. More shit?

--- TaskOrganizer_group_8/src/hvl/dat152/services/TaskWebServices.java
+++ TaskOrganizer_group_8/src/hvl/dat152/services/TaskWebServices.java
@@ -25,84 +25,85 @@ import java.util.List;
 
 /**
  * @author bki
+ *
  */
 @Path("broker")
 public class TaskWebServices {
 
-	@EJB
-	private TaskEAO taskEAO = new TaskEAO();
+    @EJB
+    private TaskEAO taskEAO = new TaskEAO();
 
-	@Path("/tasklist")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public ResponseGetTasks getTasks() {
-		ResponseGetTasks response = new ResponseGetTasks();
+    @Path("/tasklist")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseGetTasks getTasks() {
+        ResponseGetTasks response = new ResponseGetTasks();
 
-		List<Task> tasks = taskEAO.getTasks();
-		response.setTasks(tasks);
-		response.setResponseStatus(true);
-		return response;
-	}
+        List<Task> tasks = taskEAO.getTasks();
+        response.setTasks(tasks);
+        response.setResponseStatus(true);
+        return response;
+    }
 
-	@Path("/allstatuses")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public ResponseGetAllstatuses getStatuses() {
-		List<Status> allstatuses = new ArrayList<Status>();
-		for(Status statuses : Status.values()) {
-			allstatuses.add(statuses);
-		}
-		ResponseGetAllstatuses response = new ResponseGetAllstatuses();
-		response.setAllstatuses(allstatuses);
-		response.setResponseStatus(true);
-		return response;
-	}
+    @Path("/allstatuses")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseGetAllstatuses getStatuses() {
+        List<Status> allstatuses = new ArrayList<Status>();
+        for(Status statuses : Status.values()) {
+            allstatuses.add(statuses);
+        }
+        ResponseGetAllstatuses response = new ResponseGetAllstatuses();
+        response.setAllstatuses(allstatuses);
+        response.setResponseStatus(true);
+        return response;
+    }
 
-	//@PathParam("id") Integer id,
-	@Path("/task/{id}")
-	@PUT
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	//public ServerResponse UpdatedTaskStatuses(@Context UriInfo ui,@PathParam(taskId") Integer taskId, TaskStatus taskStatus) {
-	public ResponsePutTaskStatus UpdatedTaskStatuses(@PathParam("id") Integer taskId, TaskStatus status) {
-		ResponsePutTaskStatus response = new ResponsePutTaskStatus();
-		if((taskId != null) && (status != null)) {
-			taskEAO.setStatus(taskId, status.getStatus());
+    //@PathParam("id") Integer id,
+    @Path("/task/{id}")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    //public ServerResponse UpdatedTaskStatuses(@Context UriInfo ui,@PathParam(taskId") Integer taskId, TaskStatus taskStatus) {
+    public ResponsePutTaskStatus UpdatedTaskStatuses(@PathParam("id") Integer taskId, TaskStatus status) {
+        ResponsePutTaskStatus response = new ResponsePutTaskStatus();
+        if((taskId != null) && (status != null)) {
+            taskEAO.setStatus(taskId, status.getStatus());
 
-			response.setId(taskId);
-			response.setStatus(status.getStatus());
-			response.setResponseStatus(true);
-		}
-		return response;
-	}
+            response.setId(taskId);
+            response.setStatus(status.getStatus());
+            response.setResponseStatus(true);
+        }
+        return response;
+    }
 
-	@Path("/task")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public ResponseAddTask AddTask(Task task) {
-		ResponseAddTask response = new ResponseAddTask();
-		if(task != null) {
-			taskEAO.addTask(task);
+    @Path("/task")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ResponseAddTask AddTask(Task task) {
+        ResponseAddTask response = new ResponseAddTask();
+        if(task != null) {
+            taskEAO.addTask(task);
 
-			response.setTask(task);
-			response.setResponseStatus(true);
-		}
-		return response;
-	}
+            response.setTask(task);
+            response.setResponseStatus(true);
+        }
+        return response;
+    }
 
-	@Path("/task/{id}")
-	@DELETE
-	@Produces(MediaType.APPLICATION_JSON)
-	public ResponseDeleteTask deleteMember(@PathParam("id") Integer id) {
-		ResponseDeleteTask deleteResponse = new ResponseDeleteTask();
-		if(id != null) {
-			if(id >= 1) {
-				taskEAO.removeTask(id);
-				deleteResponse.setId(id);
-				deleteResponse.setResponseStatus(true);
-			}
-		}
-		return deleteResponse;
-	}
+    @Path("/task/{id}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseDeleteTask deleteMember(@PathParam("id") Integer id) {
+        ResponseDeleteTask deleteResponse = new ResponseDeleteTask();
+        if(id != null) {
+            if(id >= 1) {
+                taskEAO.removeTask(id);
+                deleteResponse.setId(id);
+                deleteResponse.setResponseStatus(true);
+            }
+        }
+        return deleteResponse;
+    }
 }
