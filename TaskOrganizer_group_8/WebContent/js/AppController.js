0735b716a24ff8cd9c6c4fe958ad45ffa7be9fff
182719 <joakim-a@hotmail.com>
2019-09-27 11:02:23 +0200

    Controller code in 1 place now

--- TaskOrganizer_group_8/WebContent/js/AppController.js
+++ TaskOrganizer_group_8/WebContent/js/AppController.js
@@ -90,6 +90,46 @@ gui.newStatusCallback = async (id,newStatus) => {
 //TODO
 window.addEventListener('load', getServerData);
 
+/*
+TASKBOX CONTROLLER STUFF.
+
+ */
+const addTaskCallback = async (task) => {
+    console.log(task);
+    try {
+        const response = await fetch('broker/task', {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        taskbox.close();
+        await response.json().then(data => {
+            console.log(data)
+            let resultTask = {
+                id: data.task.id,
+                title: data.task.title,
+                status: data.task.status
+            };
+            gui.showTask(resultTask);
+        });
+    } catch (e) {
+        console.log(`Got error ${e.message}.`)
+    }
+};
+
+const tasksmodaleboxdiv = document.getElementById("taskbox");
+const tasknewbutton = document.getElementById("newTask");
+let taskbox = new TaskBox(tasksmodaleboxdiv);
+taskbox.allstatuses = gui.allstatuses;
+taskbox.onsubmit = addTaskCallback;
+tasknewbutton.addEventListener("click", () => {
+    taskbox.show()
+}, true);
+
+
+
 //gui.newStatusCallback = (id,newStatus) => {console.log(`User has approved to change the status of task with id ${id} to ${newStatus}.`)};
 //gui.newStatusCallback = (id,newStatus) => {console.log(`Observer, task with id ${id} is not set to ${newStatus} in the view!`)};
 
